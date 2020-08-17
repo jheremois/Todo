1e33796ffd7c4cc7613c3742139112cf92f7aee7
Jheremy Castro Guerrero <jheremy802@gmail.com>
2020-08-17 00:30:11 -0400

    New colors and desing

--- code/public/js/todo.js
+++ code/public/js/todo.js
@@ -3,24 +3,57 @@ const options = document.getElementById('options')
 const new_task = document.getElementById('new_task')
 const cancel = document.getElementById('cancel')
 const textarea_task = document.getElementById('write_task')
+const sugerence = document.getElementById('sugerence')
+const section = document.getElementById('section')
+
+
 
 new_task.addEventListener('click', ()=>{
 
+    if(section.childNodes[0].id === 'sugerence'){
+     
+        sugerence.style.display = 'none'
+    
+    }
+
+
+
     options.style.display = 'none'
+    
     form.style.display = 'flex'
+    
+
+
     setTimeout(()=>{
+        
         form.style.transform = 'scale(1)'
+
     },100)
 
 })
 
+
+
 cancel.addEventListener('click', ()=>{
 
     options.style.display = 'flex'
+    
     form.style.transform = 'scale(0)'
+    
     textarea_task.value = ''
+    
+
+    
     setTimeout(()=>{
+
+        if(section.childNodes[0].id === 'sugerence'){
+            
+            sugerence.style.display = 'flex'
+        
+        }
+        
         form.style.display = 'none'
+
     },200)
 
 })
